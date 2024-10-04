1eaa3bfc7ec79f57bf141336c55dde96f43c15cd
MERP0001 <merp0001@ce.pucmm.edu.do>
2024-10-04 15:23:03 -0400

    Ui De enviar mensajes

---
+++ app/src/main/java/com/example/app_chat/activities/ui/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_chat.activities.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.app_chat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
